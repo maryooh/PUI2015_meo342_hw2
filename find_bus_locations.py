bfbdf2b70642173e1d743bcd3b35de93cb81f85e
María Ortiz <maria.ortiz@nyu.edu>
2015-09-25 11:52:10 -0400

    Update find_bus_locations.py

--- find_bus_locations.py
+++ find_bus_locations.py
@@ -2,13 +2,13 @@ import json
 import urllib2
 import sys
 
+#load json file from MTA Bus Times website using the provided developer key
 #url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=' + API_KEY +'&VehicleMonitoringDetailLevel=calls&LineRef=' + BUS_LINE
 #request = urllib2.urlopen(url)
 #data = json.loads(request.read())
 
-if __name__ == '__main__':
+if __name__=='__main__':
 	findbusloc(sys.argv[1], sys.argv[2])	
-
 	API_KEY = '9c0690a6-de1f-48a4-ab8c-8db536692b7d'
 	BUS_LINE = 'B52'
 		
@@ -19,11 +19,11 @@ def findbusloc(API_KEY, BUS_LINE):
 	vehicles = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 
 	for b in vehicles:
-			if b['PublishedLineName'] == BUS_LINE:
-				busno = b['vehicleref'][10:]
-				busLat = b['Latitude']
-				busLon = b['Longitude']
+		if b['PublishedLineName'] == BUS_LINE:
+			busno = b['vehicleref'][10:]
+			busLat = b['Latitude']
+			busLon = b['Longitude']
 
-				print 'Bus Line :%s' % (BUS_LINE)
-				print 'Number of Active Buses : %s' % len(busno)
-				print 'Bus %s is at latitute %s and longitude %s' % (busno, busLat, busLon)
+			print 'Bus Line :%s' % (BUS_LINE)
+			print 'Number of Active Buses : %s' % len(busno)
+			print 'Bus %s is at latitute %s and longitude %s' % (busno, busLat, busLon)
