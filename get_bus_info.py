dfe91bb058085b4e9f25380b0585c1ba258f53ca
María Ortiz <maria.ortiz@nyu.edu>
2015-09-25 19:13:06 -0400

    Update get_bus_info.py

--- get_bus_info.py
+++ get_bus_info.py
@@ -12,9 +12,11 @@ if __name__=='__main__':
 	BUS_LINE = 'B52'
 	
 	def getbusinfo(API_KEY, BUS_LINE):
-		url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=' + API_KEY +'&VehicleMonitoringDetailLevel=calls&LineRef=' + BUS_LINE
+		url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=' + API_KEY +
+		'&VehicleMonitoringDetailLevel=calls&LineRef=' + BUS_LINE
 		data = json.load(open(url,'r'))
-		buses = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['MonitoredVehicleJourney'][0]['OnwardCalls']['OnwardCall']['Extensions']
+		buses = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['MonitoredVehicleJourney'][0]
+		['OnwardCalls']['OnwardCall']['Extensions']
 		
 		#open csv and write rows
 		with open(sys.argv[3], 'wb') as buscsvfile:
@@ -26,11 +28,15 @@ if __name__=='__main__':
 					busLat = b['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
 					busLon = b['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
 					if len(bus['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall']) > 0:
-						busStop = bus['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
-						stopStatus = bus['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+						busStop = bus['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall']
+						[0]['StopPointName']
+						stopStatus = bus['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall']
+						[0]['Extensions']['Distances']['PresentableDistance']
 					else:
 						busStop = "N/A"
 						stopStatus = "N/A"
 					row = [busLat, busLon, busStop, stopStatus]
-					writer.writerow(row)busStop = b['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
-					stopstatus= b['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+					writer.writerow(row)busStop = b['MonitoredVehicleJourney']['OnwardCalls']
+					['OnwardCall'][0]['StopPointName']
+					stopstatus= b['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall']
+					[0]['Extensions']['Distances']['PresentableDistance']
