0fbb4ea2d0ea196d5a82c5147cd44a519bab5873
María Ortiz <maria.ortiz@nyu.edu>
2015-09-25 17:41:36 -0400

    Update find_bus_locations.py

--- find_bus_locations.py
+++ find_bus_locations.py
@@ -9,10 +9,10 @@ import sys
 
 if __name__=='__main__':
 	findbusloc(sys.argv[1], sys.argv[2])	
-	API_KEY = '9c0690a6-de1f-48a4-ab8c-8db536692b7d'
-	BUS_LINE = 'B52'
-		
+	
 	def findbusloc(API_KEY, BUS_LINE):
+		API_KEY = '9c0690a6-de1f-48a4-ab8c-8db536692b7d'
+		BUS_LINE = 'B52'
 		url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s' % (API_KEY, BUS_LINE)
 		#jsonFile = open(url, 'r')
 		data = json.load(open(url,'r'))
@@ -21,8 +21,8 @@ if __name__=='__main__':
 		for b in vehicles:
 			if b['PublishedLineName'] == BUS_LINE:
 				busno = b['vehicleref'][10:]
-				busLat = b['Latitude']
-				busLon = b['Longitude']
+				busLat = b['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+				busLon = b['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
 
 				print 'Bus Line :%s' % (BUS_LINE)
 				print 'Number of Active Buses : %s' % len(busno)
